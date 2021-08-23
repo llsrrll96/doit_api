1a903e18a49c9476272fb3a82ace528f6dbe3580
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2021-08-23 23:53:12 +0900

    update

--- app.py
+++ app.py
@@ -2,70 +2,31 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 
 from Controller.hello import HelloWorld
+from Controller.hello import Test
 from Controller.Item import Item
+from Controller.find_controller import Find_Controller
+import pandas as pd
 
 #Flask 객체 인스턴스 생성
+from Repository.database import DB
+from Service.search_service import Search
+
 app = Flask(__name__)
 api = Api(app)
 
+db = DB()
+search =Search(db.get_data())
 
+api.add_resource(Item, '/api/item/<string:name>')
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
+api.add_resource(Test,'/api/test/<string:name>',
+                 resource_class_kwargs={'search':search})
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+api.add_resource(Find_Controller,'/api/search/<string:name>',
+                 resource_class_kwargs={'search':search})
 
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
-# Todo
-#   show a single todo item and lets you delete them
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-#   shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    def get(self):
-        return TODOS
-
-    def post(self):
-        args = parser.parse_args()
-        todo_id = 'todo%d' % (len(TODOS) + 1)
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
-
-##
-## Actually setup the Api resource routing here
-##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<string:todo_id>')
-
-
-
-# api.add_resource(Dummy, '/', '/index')
-
-api.add_resource(Item, '/item/<string:name>')
 api.add_resource(HelloWorld,'/')
+# api.add_resource(Dummy, '/', '/index')
 
 if __name__=="__main__":
     app.run()
